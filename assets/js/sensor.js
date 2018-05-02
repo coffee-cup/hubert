8461e3964a96bf2f27ba69e36759d095d6b71792
Jake Runzer <jakerunzer@gmail.com>
2018-05-02 03:27:35 -0700

    very basic plotly.js graphs

--- assets/js/sensor.js
+++ assets/js/sensor.js
@@ -1,14 +1,39 @@
-const sensorGraph = sensor => document.querySelector(`#${sensor.slug}-graph`);
+import Plotly from 'plotly.js/lib/core';
+
+const sensorGraphParent = sensor =>
+  document.querySelector(`#${sensor.slug}-graph`);
+const sensorGraph = sensor => sensorGraphParent(sensor).querySelector('.graph');
+
+const createGraph = sensor => {
+  const x = sensor.points.map(p => p.date);
+  const y = sensor.points.map(p => p.value);
+  const data = [{ x, y, type: 'scatter' }];
+
+  const m = 4;
+  const layout = {
+    autosize: true,
+    yaxis: { range: [0, 100] }
+  };
+
+  Plotly.newPlot(sensorGraph(sensor), data, layout);
+};
 
 const sensorUpdate = (sensor, val) => {
   console.log(`${sensor.name}`);
   console.log(val);
 
-  const points = sensorGraph(sensor).querySelector('.points');
-  const newPoint = document.createElement('p');
-  newPoint.innerHTML = JSON.stringify(val);
+  // const points = sensorGraphParent(sensor).querySelector('.points');
+  // const newPoint = document.createElement('p');
+  // newPoint.innerHTML = JSON.stringify(val);
+  // points.insertBefore(newPoint, points.firstChild);
 
-  points.insertBefore(newPoint, points.firstChild);
+  const update = {
+    x: [[val.date]],
+    y: [[val.value]]
+  };
+  console.log(update);
+
+  Plotly.extendTraces(sensorGraph(sensor), update, [0]);
 };
 
 const registerChannelUpdates = (socket, sensor) => {
@@ -23,4 +48,7 @@ const registerChannelUpdates = (socket, sensor) => {
 
 export const handleSensor = (socket, sensor) => {
   registerChannelUpdates(socket, sensor);
+
+  // setTimeout(() => createGraph(sensor), 2000);
+  createGraph(sensor);
 };
