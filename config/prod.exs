90c3d6c4ac3fe3058cf6359b552536c1e1e0b2de
Jake Runzer <jakerunzer@gmail.com>
2018-05-04 14:49:15 -0700

    fix syntax error in prod config

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :hubert, HubertWeb.Endpoint,
 # Configure database
 config :hubert, Hubert.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: {:system "DATABASE_URL"},
+  url: {:system, "DATABASE_URL"},
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
